e67b5d7440e7b3e5f11d842a3eeb4ab628f1bca6
Eider Prado <eiderprado@hotmail.com>
2020-04-27 04:33:12 -0500

    Agregar Api

---
+++ PNN.Web/Controllers/API/ContentTypesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PNN.Web.Data.Entities;
+using PNN.web.Data;
+
+namespace PNN.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentTypesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ContentTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // trae la lista de contentType: api/ContentTypes
+        [HttpGet]
+        public IEnumerable<ContentType> GetContentTypes()
+        {
+            return _context.ContentTypes;
+        }
+    }
+}
