de2792dd048643e382d99dd8816344b527a653d0
Eider Prado <eiderprado@hotmail.com>
2020-04-27 04:44:54 -0500

    Ok - Api UserController

---
+++ PNN.Web/Controllers/API/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PNN.Common.Models;
+using PNN.web.Data;
+
+namespace PNN.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost]
+        [Route("GetOwnerByEmail")]
+        public async Task<IActionResult> GetOwner(EmailRequest emailRequest)
+        {
+            
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.Email == emailRequest.Email);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
