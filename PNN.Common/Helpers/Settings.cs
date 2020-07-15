d65fe5139162e5c8a3f6754ce0a8391b661cbad6
lee21_8@hotmail.com <lee21_8@hotmail.com>
2020-07-14 20:04:43 -0500

    1. se añade la implementacion de codigo par ael uso de la clase syncfusion rotaros

---
+++ PNN.Common/Helpers/Settings.cs
@@ -0,0 +1,24 @@
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PNN.Common.Helpers
+{
+    class Settings
+    {
+        private const string _propertyImages = "PropertyImages";
+        private static readonly string _settingsDefault = string.Empty;
+
+        private static ISettings AppSettings => CrossSettings.Current;
+
+        public static string PropertyImages
+        {
+            get => AppSettings.GetValueOrDefault(_propertyImages, _settingsDefault);
+            set => AppSettings.AddOrUpdateValue(_propertyImages, value);
+        }
+
+
+    }
+}
---
+++ PNN.Common/Models/RotatorModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PNN.Common.Models
+{
+    public class RotatorModel
+    {
+        public string Image { get; set; }
+
+    }
+}
